7b4b533c82c715212f3640687efd49b28eccb466
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-01-26 22:11:33 +0100

    Initial commit

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/*public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String MARCO = "YOURNAME";
+        Number Pi = 3.141590;
+        System.out.printf("%s",MARCO);
+        System.out.println(" ");
+        System.out.printf("%f",Pi);
+    }*/
+
+
+
+    import java.util.Scanner;
+
+
+
+
---
+++ src/start.java
@@ -0,0 +1,8 @@
+public class start {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("set Your Age");
+            int age = scanner.nextInt();
+            System.out.printf("my age is %s",age);
+
+    }
+}
